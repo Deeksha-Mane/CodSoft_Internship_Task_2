51019e8a2a4af1ce10ec2e1bb7e2b97314b87f37
Deeksha Mane <manediksha264@gmail.com>
2024-08-26 16:05:18 +0530

    new file:   Amazon Website Video.mp4 	new file:   Codexa Video.mp4 	new file:   Deeksha_Mane_Resume.pdf 	new file:   LinkedIn.png 	new file:   Simon Say Game.mp4 	new file:   Spotify Clone Video.mp4 	new file:   bg-img.jpg 	new file:   bg-img4.jpg     new file:   c logo.png 	new file:   css logo.png 	new file:   foot-img.jfif 	new file:   github.png 	new file:   html logo.png 	new file:   index.html 	new file:   java.logo.png 	new file:   js logo.png 	new file:   leetcode.png 	new file:   my-portfolio-image-1.jpeg 	new file:   my_portfolio_logo.png 	new file:   python logo.jfif 	new file:   script.js 	new file:   style.css

---
+++ script.js
@@ -0,0 +1,9 @@
+//Using JavaScript for adding funcionality to the sidepanel
+
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
+}
+  
+function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+}
